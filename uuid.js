489e31eaac1bacef9234ec55668e1dd0d21844fe
sazix <86483949+Sazix1337@users.noreply.github.com>
2022-06-15 11:39:39 +0300

    Delete uuid.js

--- uuid.js
+++
@@ -1,9 +0,0 @@
-const uuid = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        const rand = Math.random() * 16 | 0
-        const uuid = (c == 'x') ? rand : (rand & 0x3 | 0x8)
-        return uuid.toString(16) 
-    })
-} 
-
-module.exports = uuid
